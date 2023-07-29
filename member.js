3b2586b0c35e75efb361b199652646d0002de944
Ayesha khoja <106901835+Ayesha-khoja@users.noreply.github.com>
2023-07-29 19:48:49 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember()
+{
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var memberList = document.getElementById("memberList");
+    var skillsList = document.getElementById("skillsList");
+    var projectsList = document.getElementById("projectsList");
+    var contactList = document.getElementById("contactList");
+    var aboutList = document.
+
